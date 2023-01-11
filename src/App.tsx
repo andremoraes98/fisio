59a1204f2b1eda4261f3f7e1fdb790fa475e52c5
Andre Moraes <andre.moraes.98@gmail.com>
2023-01-11 09:40:58 -0300

    feat: adição do path para renderizar o componente Calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
+import Calendar from './pages/Calendar/Calendar';
 import Login from './pages/Login/Login';
 import Redirect from './pages/Redirect';
 
@@ -7,6 +8,7 @@ function App() {
 	return (
 		<Routes>
 			<Route path='/login' element={<Login />}/>
+			<Route path='/calendar' element={<Calendar />}/>
 			<Route path='/' element={<Redirect />}/>
 		</Routes>
 	);

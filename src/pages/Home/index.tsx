bd71d05c18331625b6951c39a1d1e8babe3c23c5
Andre Moraes <andre.moraes.98@gmail.com>
2023-01-14 09:16:30 -0300

    feat: criação do component Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,57 @@
+import React, {type FC} from 'react';
+import Button from 'react-bootstrap/Button';
+import {useNavigate} from 'react-router-dom';
+import './Home.css';
+
+const Home: FC = () => {
+	const navigate = useNavigate();
+	return (
+		<section id='home-content'>
+			<div className='flex-center-around m-3'>
+				<div className='home-button'>
+					<Button
+						onClick={() => {
+							navigate('/select-customer');
+						}}
+					>
+						Selecionar aluno
+					</Button>
+				</div>
+
+				<div className='home-button'>
+					<Button
+						onClick={() => {
+							navigate('/create-customer');
+						}}
+					>
+						Cadastrar aluno
+					</Button>
+				</div>
+			</div>
+
+			<div className='flex-center-around m-3'>
+				<div className='home-button'>
+					<Button
+						onClick={() => {
+							navigate('/calendar');
+						}}
+					>
+					Calcular pagamento
+					</Button>
+				</div>
+
+				<div className='home-button'>
+					<Button
+						onClick={() => {
+							navigate('/edit-customer');
+						}}
+					>
+					Editar aluno
+					</Button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Home;
